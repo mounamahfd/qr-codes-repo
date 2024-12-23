8faad10cb1f829dade8b654ee837bb102f7ae04b
mounamahfd <22074@supnum.mr>
2024-12-23 20:55:52 +0000

    Updates

---
+++ _next/static/chunks/app/layout-1d3fc4114d0c80ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6007:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,340,23)),Promise.resolve().then(l.t.bind(l,347,23))},347:()=>{},340:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_4196b9"}}},e=>{var s=s=>e(e.s=s);e.O(0,[758,441,517,358],()=>s(6007)),_N_E=e.O()}]);
